692a9440c8db483107235074d50de656eb8f7ff3
Uttampatel1 <86312108+Uttampatel1@users.noreply.github.com>
2025-07-14 22:15:55 +0530

    Update URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,23 @@ import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
-  base: "/isharpanam";
+
   return {
+    base: '/isharpanam/',
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
-      }
+      },
     },
     server: {
-      allowedHosts: [
-        'isharpanam.github.io'
-      ]
-    }
+      origin: 'https://isharpanam.github.io',
+      cors: true,
+      strictPort: true,
+      host: true,
+    },
   };
 });
